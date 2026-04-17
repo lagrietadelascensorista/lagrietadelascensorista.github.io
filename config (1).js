533a861c7d880b5e1ef90d461c29eb8333c93e89
lagrietadelascensorista <joanperello18@gmail.com>
2026-04-17 10:52:45 +0200

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,21 @@
+// config.js — Configuración global automática
+// Sube este archivo a GitHub en la misma carpeta que index.html
+// Todos los dispositivos se configurarán automáticamente al abrir la app
+// Generado: 17/4/2026, 10:52:23
+
+window.DRIVE_CONFIG = {
+  apiKey:     'AIzaSyBZtopBU4WTxX0laxV_97niIcEdCphBoxY',
+  folderId:   '1KNNzGdc69EMFXcTER7xPyh9GqSgx9ybh',
+  defaultCat: 'General'
+};
+
+window.FB_CONFIG = {
+  "apiKey": "AIzaSyDyoz-9ga2c-e7JyT",
+  "authDomain": "lagrietadela",
+  "projectId": "lagrietadela",
+  "storageBucket": "lagrietadela",
+  "messagingSenderId": "639129926720",
+  "appId": "1:639129926720:web:83e335def78ac7ec78f3a5"
+};
+
+window.FB_APP_ID = 'grieta-ascensorista';
